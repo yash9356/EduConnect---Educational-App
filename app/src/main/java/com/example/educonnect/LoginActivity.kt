0b07addf6b62ac2bb40d90a0af0bf2ce447e30e1
Yash_Patil <yashpatil7622@gmail.com>
2023-09-29 14:17:12 +0530

    Add login check code

--- app/src/main/java/com/example/educonnect/LoginActivity.kt
+++ app/src/main/java/com/example/educonnect/LoginActivity.kt
@@ -1,12 +1,22 @@
 package com.example.educonnect
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.educonnect.ui.HomeActivity
 import com.example.educonnect_educationalapp.R
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.ktx.Firebase
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        val auth = FirebaseAuth.getInstance()
+        if (auth.currentUser!=null){
+            startActivity(Intent(this, HomeActivity::class.java))
+            this.finish()
+        }else{
+            setContentView(R.layout.activity_login)
+        }
     }
 }
